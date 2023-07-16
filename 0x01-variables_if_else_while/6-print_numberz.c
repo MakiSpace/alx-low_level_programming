2e827b76e4c4270860484d424e42b7234228cf01
MakiSpace <kayawem@gmail.com>
2023-07-16 02:01:06 -0700

    ./6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all single digit numbers of base ten
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+
+}
